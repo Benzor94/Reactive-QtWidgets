f77b7451f7c48d028cb64caa879e5ef88bd00027
Bence Racskó <bracsko94@gmail.com>
2026-05-22 23:45:39 +0200

    added exporting

--- src/reactive_qtwidgets/__init__.py
+++ src/reactive_qtwidgets/__init__.py
@@ -1,5 +1,7 @@
-from _observable_property import ObservableProperty
+from reactive_qtwidgets._derived_property import DerivedProperty
+from reactive_qtwidgets._observable_property import ObservableProperty
 
 __all__ = [
+    'DerivedProperty',
     'ObservableProperty',
 ]
